50c8efdacd11c73b21f4c8b3a15987b607b2aa13
youcef <youcefabdellani@gmail.com>
2025-06-11 18:43:21 +0100

    rename hello_world to string

---
+++ strings.py
@@ -0,0 +1,12 @@
+name = "youcef"
+age = 30
+height = 5.4
+
+print(name, age, height)
+
+greeting = "     Hello Youcef I am HERE!     "
+print(len(greeting))
+print(greeting.strip())
+print(greeting.strip().upper())
+print(greeting.strip().lower())
+print(greeting.strip().title())
