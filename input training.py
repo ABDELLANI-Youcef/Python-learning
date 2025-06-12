4f06539aed930f47c103259406831cb60c272690
youcef <youcefabdellani@gmail.com>
2025-06-12 08:55:40 +0100

    add input training file

---
+++ input training.py
@@ -0,0 +1,7 @@
+name = input("Enter your name please: ")
+score = int(input("enter your score please: "))
+print(f"Hello {name}, your score is {score}.")
+
+age1 = int(input("Enter your age please: "))
+age2 = float(input("Enter your height in meters please: "))
+print(f"the sum of age1 : {age1} and age2 : {age2} is {age1 + age2}")
