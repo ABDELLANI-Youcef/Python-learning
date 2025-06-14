90265c8ea5966a25fa50e716b174c810a31e7fee
youcef <youcefabdellani@gmail.com>
2025-06-14 13:31:52 +0100

    add a demostration on the the creationand the access of the attributes of class

--- OOP_concepts/classes_concept.py
+++ OOP_concepts/classes_concept.py
@@ -10,6 +10,16 @@ class Developer:
 def main():
   youcef = Developer("Youcef", "Python", 5)
   print(youcef.into())
+  # Output: Name: Youcef, Language: Python, Experience: 5 years
+  print(f"{youcef.name} is a developer with {youcef.experience} years of experience in {youcef.languege}.")
+
+  youcef.experience = 11
+  print(f"Nom, after 6 years {youcef.name} is a developer with {youcef.experience} years of experience in {youcef.languege}.")
+
+  ahmed = Developer("Ahmed", "JavaScript", 3)
+  print(ahmed.into())
+  # Output: Name: Ahmed, Language: JavaScript, Experience: 3 years
+  print(f"{ahmed.name} is a developer with {ahmed.experience} years of experience in {ahmed.languege}.")
   return
 
 if __name__ == "__main__":
