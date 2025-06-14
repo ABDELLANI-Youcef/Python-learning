b8c5baba6b2cdda54e28dff14ec7a45ce9265a20
youcef <youcefabdellani@gmail.com>
2025-06-14 16:38:50 +0100

    update the Fetching function to use the url request instead urllib3

--- data fetching/usig_urllib.py
+++ data fetching/usig_urllib.py
@@ -1,33 +1,18 @@
-import urllib3
+from urllib import request
 import json
 from rich import print_json
 
 
-# def get_posts():
-#   url = "https://jsonplaceholder.typicode.com/posts"
-#   try:
-#     with urllib3.request.open(url) as response:
-#       data = response.read()
-#       parsed_data = json.loads(data)
-#       return parsed_data
-#   except:
-#     print(f"[red]Error fetching data: {e}[/red]")
-#     return
-
 def get_posts():
   url = "https://jsonplaceholder.typicode.com/posts"
-  http = urllib3.PoolManager()
   try:
-    response = http.request('GET', url)
-    if response.status == 200:
-      data = response.data.decode('utf-8')
-      parsed_data = json.loads(data)
-      return parsed_data
-    else:
-      print(f"[red]Error fetching data: {response.status}[/red]")
-      return []
+    response = request.urlopen(url)
+    data = response.read()
+    posts = json.loads(data)
+    return posts
+
   except Exception as e:
-    print(f"[red]Error fetching data: {e}[/red]")
+    print(f"Error decoding JSON: {e}")
     return []
 
 def main():
