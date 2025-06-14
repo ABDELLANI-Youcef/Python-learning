bff61f7713da9cc08fe7b13ced9e857bf1c4fd53
youcef <youcefabdellani@gmail.com>
2025-06-14 13:19:05 +0100

    add the classes concept demonstration

---
+++ OOP_concepts/classes_concept.py
@@ -0,0 +1,16 @@
+class Developer:
+  def __init__(self, nale, languege, experience):
+    self.name = nale
+    self.languege = languege
+    self.experience = experience
+
+  def into(self):
+    return f"Name: {self.name}, Language: {self.languege}, Experience: {self.experience} years"
+
+def main():
+  youcef = Developer("Youcef", "Python", 5)
+  print(youcef.into())
+  return
+
+if __name__ == "__main__":
+  main()
