ab7d3d978b3ff93c3289af6f9403595da0807d80
youcef <youcefabdellani@gmail.com>
2025-06-14 16:50:03 +0100

    add url fetching method using the requests package

---
+++ data fetching/using_request.py
@@ -0,0 +1,16 @@
+import requests
+from rich import print_json
+
+def get_posts(url):
+  response = requests.get(url)
+
+  parsed_json = response.json()
+  return parsed_json
+
+def main():
+  url = "https://jsonplaceholder.typicode.com/posts"
+  posts = get_posts(url)
+  print_json(data=posts, indent=2)
+
+if __name__ == "__main__":
+ main()
