70027ca1090ee44805f33db51998a1a4de575727
youcef <youcefabdellani@gmail.com>
2025-06-14 16:57:59 +0100

    add the demonstration for the query parameters in the url requests

--- data fetching/query_params.py
+++ data fetching/query_params.py
@@ -1,15 +1,16 @@
 import requests
 from rich import print_json
 
-def get_posts(url):
-  response = requests.get(url)
+def get_posts(url, user_id = 7):
+  user_params = {'userId': user_id}
+  response = requests.get(url, user_params)
 
   parsed_json = response.json()
   return parsed_json
 
 def main():
   url = "https://jsonplaceholder.typicode.com/posts"
-  posts = get_posts(url)
+  posts = get_posts(url, 4)
   print_json(data=posts, indent=2)
 
 if __name__ == "__main__":
