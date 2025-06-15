92514fdf629e96cbe5b6f1a9e2e49c2aad936cc4
youcef <youcefabdellani@gmail.com>
2025-06-15 09:41:54 +0100

    add new route with path parameter

--- api_project/app.py
+++ api_project/app.py
@@ -1,8 +1,12 @@
-from bottle import route, run
+from bottle import route, run, response
 
 @route('/')
 def home():
   return "Welcome to the API Project!"
+
+@route('/hello/<name>')
+def hello(name):
+  return f"Hello, {name}! youcef are amazing!"
 
 if __name__ == "__main__":
   run(host='localhost', port=8080, debug=True, reloader=True)
