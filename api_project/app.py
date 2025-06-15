9e1e1c32ffdc5250b240cb96ea5fa73d2356090e
youcef <youcefabdellani@gmail.com>
2025-06-15 09:48:45 +0100

    add route with json response

--- api_project/app.py
+++ api_project/app.py
@@ -1,5 +1,13 @@
 from bottle import route, run, response
 
+countries = [
+  {"name": "Algeria", "capital": "Algiers", "population": 43851044},
+  {"name": "Egypt", "capital": "Cairo", "population": 104124440},
+  {"name": "Morocco", "capital": "Rabat", "population": 36910560},
+  {"name": "Tunisia", "capital": "Tunis", "population": 11818619},
+  {"name": "Libya", "capital": "Tripoli", "population": 6871292}
+]
+
 @route('/')
 def home():
   return "Welcome to the API Project!"
@@ -7,6 +15,11 @@ def home():
 @route('/hello/<name>')
 def hello(name):
   return f"Hello, {name}! youcef are amazing!"
+
+@route('/countries')
+def get_countries():
+  response.content_type = 'application/json'
+  return {"countries": countries}
 
 if __name__ == "__main__":
   run(host='localhost', port=8080, debug=True, reloader=True)
